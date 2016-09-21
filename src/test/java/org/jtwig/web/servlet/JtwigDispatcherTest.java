37247baaa22d572b2ba3461cbd503748a7dfe75b
Joao De Melo <jmelo@lyncode.com>
2016-09-21 21:03:33 +0100

    Causing web resources to throw an exception when resources are not found as per jtwig/jtwig#317

--- src/test/java/org/jtwig/web/servlet/JtwigDispatcherTest.java
+++ src/test/java/org/jtwig/web/servlet/JtwigDispatcherTest.java
@@ -1,6 +1,8 @@
 package org.jtwig.web.servlet;
 
 import org.jtwig.environment.Environment;
+import org.jtwig.resource.exceptions.ResourceNotFoundException;
+import org.jtwig.resource.metadata.ResourceMetadata;
 import org.jtwig.resource.reference.ResourceReference;
 import org.jtwig.web.resource.WebResourceLoader;
 import org.jtwig.web.servlet.model.factory.ApplicationFactory;
@@ -27,11 +29,28 @@ public class JtwigDispatcherTest {
         JtwigDispatcher underTest = new JtwigDispatcher(environment, resourceReference, applicationFactory);
         HttpServletRequest request = mock(HttpServletRequest.class);
         HttpServletResponse response = mock(HttpServletResponse.class);
+        ResourceMetadata resourceMetadata = mock(ResourceMetadata.class);
 
+        when(environment.getResourceEnvironment().getResourceService().loadMetadata(resourceReference)).thenReturn(resourceMetadata);
+        when(resourceMetadata.exists()).thenReturn(true);
         when(request.getAttributeNames()).thenReturn(new Vector<String>().elements());
 
         underTest.render(request, response);
 
         verify(applicationFactory).create(request);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void resourceNotFound() throws Exception {
+        JtwigDispatcher underTest = new JtwigDispatcher(environment, resourceReference, applicationFactory);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        ResourceMetadata resourceMetadata = mock(ResourceMetadata.class);
+
+        when(environment.getResourceEnvironment().getResourceService().loadMetadata(resourceReference)).thenReturn(resourceMetadata);
+        when(resourceMetadata.exists()).thenReturn(false);
+        when(request.getAttributeNames()).thenReturn(new Vector<String>().elements());
+
+        underTest.render(request, response);
     }
 }
